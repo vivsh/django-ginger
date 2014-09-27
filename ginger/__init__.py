b434663848112d8ce28f9d167a27399ff7965f0e
vivsh <vivek@17thstep.com>
2014-09-27 14:29:29 +0530

    added basic setup

---
+++ ginger/__init__.py
@@ -0,0 +1,4 @@
+__author__ = 'shodh'
+
+def say_name():
+    print("Hello World")
--- setup.py
+++ setup.py
@@ -1,1 +1,38 @@
-form setuptools import setup
+
+from os import path
+from setuptools import setup, find_packages
+
+base_dir = path.abspath(path.dirname(__file__))
+
+with open(path.join(base_dir, "DESCRIPTION.rst")) as f:
+    long_description = f.read()
+
+setup(
+    name="django_ginger",
+    description="Set of django utilities",
+    long_description=long_description,
+    version="0.1",
+    url="https://github.com/vivsh/django-ginger",
+    author="Vivek Sharma",
+    author_email="vivek@17thstep.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4"
+    ],
+    keywords="django utilities",
+    install_requires=[
+        "django>=1.7",
+        "jinja2",
+        "django_jinja",
+        "pillow",
+    ],
+    packages=[
+        "ginger",
+    ],
+)
