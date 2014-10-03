25168dab1097cf49793e0252cf9254c53ca2b300
vivsh <vivek@17thstep.com>
2014-10-03 15:59:30 +0530

    removed venv folder

--- ginger/apps.py
+++ ginger/apps.py
@@ -11,5 +11,6 @@ class GingerConfig(AppConfig):
         from django.conf import settings
         if 'django_jinja' not in settings.INSTALLED_APPS:
             from django_jinja import base
-            base.env.add_extension('ginger.extensions.URLExtension')
+            from ginger.extensions import URLExtension
+            base.env.add_extension(URLExtension)
             base.env.initialize()
