d6b08efddc4d71d5b61e43bc27f6cbb6e1191166
vivsh <vivek@17thstep.com>
2014-10-22 13:52:37 +0530

    added height model field

---
+++ ginger/models/fields.py
@@ -0,0 +1,17 @@
+
+import random
+from django.db import models
+
+from ginger.forms import HeightField as HeightFormField
+
+
+__all__ = ['HeightField']
+
+
+class HeightField(models.PositiveIntegerField):
+
+    def formfield(self, **kwargs):
+        defaults = {'form_class': HeightFormField}
+        defaults.update(kwargs)
+        return models.PositiveIntegerField.formfield(self, **defaults)
+
