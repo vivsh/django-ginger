7ee74bb97452f90330e791046ef61d18950db3b7
vivsh <vivek@17thstep.com>
2015-04-21 12:58:32 +0530

    changed truncate

--- ginger/management/commands/truncate.py
+++ ginger/management/commands/truncate.py
@@ -9,40 +9,29 @@ from django.core.management import BaseCommand, CommandError
 
 class Command(BaseCommand):
 
-    args = "app_name.model_name"
-    help = 'Create data for the specified model'
-
     option_list = (
-        make_option(
-            "-t",
-            "--total",
-            default=20,
-            type="int",
-            help="Total number of instances to be created"
-        ),
-        make_option(
-            "-p",
-            "--pretense",
-            default=None,
-            help="Pretense to be used for content generation"
-        ),
+
     ) + BaseCommand.option_list
+
+    def handle(self, *args, **options):
+        if not settings.DEBUG:
+            raise CommandError("This command cannot be run in production environment")
+        if len(args) == 1 and "." in args[0]:
+            name = args[0]
+            app_label, model_name = name.split(".", 1)
+            model = apps.get_model(app_label, model_name)
+            self.truncate_model(model)
+        elif not args:
+            for app in apps.get_app_configs():
+                self.truncate_app(app)
+        else:
+            for name in args:
+                app = apps.get_app_config(name)
+                self.truncate_app(app)
+
+    def truncate_app(self, app):
+        models = app.get_models()
+        map(self.truncate_model, models)
 
     def truncate_model(self, model):
         model.objects.all().delete()
-
-    def handle(self, *args, **options):
-        if not args:
-            raise CommandError("No model specified")
-        name = args[0]
-        if len(args) > 1 :
-            raise CommandError("Too many arguments")
-        if not settings.DEBUG:
-            raise CommandError("This command cannot be run in production environment")
-        if "." in name:
-            app_label, model_name = name.split(".", 1)
-            models = [apps.get_model(app_label, model_name)]
-        else:
-            app = apps.get_app_config(name)
-            models = app.get_models()
-        map(self.truncate_model, models)
