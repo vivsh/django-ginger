324ad283090fd2b4803a3df5902307d0ab420ffe
vivsh <vivek@17thstep.com>
2015-11-21 09:40:55 +0530

    removed deprecated settings

--- ginger/conf/settings.py
+++ ginger/conf/settings.py
@@ -24,11 +24,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
-
-TEMPLATE_CONTEXT_PROCESSORS += (
-    'django.core.context_processors.request',
-)
-
 JINJA2_EXCLUDED_FOLDERS = ["admin", "debug_toolbar", "suit", 'rest_framework']
 
 JINJA2_AUTOESCAPE = False
