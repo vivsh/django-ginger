b92a3c0acbdb5347899f2a8bd934014cd3396cf3
vivsh <vivek@17thstep.com>
2016-02-10 15:26:10 +0530

    django 1.9 changes

--- ginger/conf/settings.py
+++ ginger/conf/settings.py
@@ -47,7 +47,7 @@ GINGER_TEMPLATE_CONTEXT_PROCESSORS = (
     "django.template.context_processors.static",
     "django.template.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    'django.core.context_processors.request'
+    'django.template.context_processors.request'
 )
 
 
